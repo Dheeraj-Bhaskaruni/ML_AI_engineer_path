38dc9f19c345980cb6c47be38a1430df2b0faad6
Dheeraj Bhaskaruni <dheeraj58799@gmail.com>
2025-10-17 10:40:54 -0500

    revise Linear reg

--- Projects/Linear_reg_Honey_Production.py
+++ Projects/Linear_reg_Honey_Production.py
@@ -1,29 +1,66 @@
-import codecademylib3_seaborn
+# import codecademylib3_seaborn
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from sklearn import linear_model
+#
+# df = pd.read_csv("https://content.codecademy.com/programs/data-science-path/linear_regression/honeyproduction.csv")
+#
+# print(df.head())
+#
+# prod_per_year = df.groupby('year').totalprod.mean().reset_index()
+#
+# X = prod_per_year["year"]
+# X = X.values.reshape(-1, 1)
+#
+# print(X)
+#
+# y = prod_per_year['totalprod']
+#
+# plt.scatter(X,y)
+# plt.show()
+#
+# regr = linear_model.LinearRegression()
+#
+# regr.fit(X,y)
+#
+# print(regr.coef_[0])
+# print(regr.intercept_)
+#
+# y_predict = regr.predict(X)
+#
+# plt.plot(X,y_predict)
+# plt.show()
+#
+# X_future = np.array(range(2013,2051))
+# X_future = X_future.reshape(-1,1)
+#
+# future_predict = regr.predict(X_future)
+#
+# plt.scatter(X_future, future_predict)
+# plt.show()
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import linear_model
-
-df = pd.read_csv("https://content.codecademy.com/programs/data-science-path/linear_regression/honeyproduction.csv")
-
-print(df.head())
-
+from sklearn.linear_model import LinearRegression
+import matplotlib
+import matplotlib.pyplot as plt
+df = pd.read_csv('/Users/dheeraj_bhaskaruni/Documents/Prep/ML_AI_engineer_path/datasets/honeyproduction.csv')
+matplotlib.use("TkAgg")
 prod_per_year = df.groupby('year').totalprod.mean().reset_index()
 
-X = prod_per_year["year"]
-X = X.values.reshape(-1, 1)
+print(prod_per_year)
 
-print(X)
+X = prod_per_year['year']
+X = X.values.reshape(-1,1)
 
 y = prod_per_year['totalprod']
 
 plt.scatter(X,y)
 plt.show()
 
-regr = linear_model.LinearRegression()
+regr = LinearRegression()
 
 regr.fit(X,y)
-
 print(regr.coef_[0])
 print(regr.intercept_)
 
@@ -32,10 +69,10 @@ y_predict = regr.predict(X)
 plt.plot(X,y_predict)
 plt.show()
 
-X_future = np.array(range(2013,2051))
+X_future = np.array(range(2014,2051))
 X_future = X_future.reshape(-1,1)
 
 future_predict = regr.predict(X_future)
 
-plt.scatter(X_future, future_predict)
+plt.plot(X_future, future_predict)
 plt.show()
