48eb8bf7f8b99fe8625d10c47070cdd160e4c7c2
Dheeraj Bhaskaruni <dheeraj58799@gmail.com>
2025-10-25 16:46:41 -0500

    udpated

--- k_nearest_neighbors/distance_bw_2d.py
+++ k_nearest_neighbors/distance_bw_2d.py
@@ -1,11 +1,15 @@
-star_wars = [125, 1977]
-raiders = [115, 1981]
-mean_girls = [97, 2004]
+star_wars = [125, 1977, 11000000]
+raiders = [115, 1981, 18000000]
+mean_girls = [97, 2004, 17000000]
 
 def distance(movie1, movie2):
-  return (((movie1[0] -movie2[0])**2) + ((movie1[1] -movie2[1])**2)) ** 0.5
+  su = 0
+  distance = 0
+  for i in range(len(movie1)):
+    su = su + (movie1[i] - movie2[i]) **2
+  distance = su ** 0.5
+  return distance
 
 
 print(distance(star_wars, raiders))
-
 print(distance(star_wars, mean_girls))
