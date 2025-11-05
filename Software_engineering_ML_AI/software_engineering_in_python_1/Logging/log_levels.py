ddf14ae547c85aebc1702c0113e9aaef26265da3
Dheeraj Bhaskaruni <dheeraj58799@gmail.com>
2025-11-05 15:04:03 -0600

    log_levels

---
+++ Software_engineering_ML_AI/software_engineering_in_python_1/Logging/log_levels.py
@@ -0,0 +1,18 @@
+import logging
+print(logging.DEBUG)
+print(logging.INFO)
+print(logging.WARNING)
+print(logging.ERROR)
+print(logging.CRITICAL)
+print(logging.NOTSET)
+
+
+'''
+o/p's:
+10
+20
+30
+40
+50
+0
+'''
