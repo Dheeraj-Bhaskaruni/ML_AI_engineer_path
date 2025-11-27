ccbc0c3f1a91f2c58a7527f485825ea0413d5211
Dheeraj Bhaskaruni <dheeraj58799@gmail.com>
2025-11-27 11:17:51 -0600

    updated

--- decision_tree/implementing_decision_tree.py
+++ decision_tree/implementing_decision_tree.py
@@ -1,38 +1,74 @@
+import codecademylib3
+import seaborn as sns
 import pandas as pd
 import numpy as np
-import codecademylib3
-import matplotlib.pyplot as plt
-
-
-#Import models from scikit learn module:
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
-#Loading the dataset
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data', names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'accep'])
+#https://archive.ics.uci.edu/ml/machine-learning-databases/flags/flag.data
+cols = ['name','landmass','zone', 'area', 'population', 'language','religion','bars','stripes','colours',
+'red','green','blue','gold','white','black','orange','mainhue','circles',
+'crosses','saltires','quarters','sunstars','crescent','triangle','icon','animate','text','topleft','botright']
+df= pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/flags/flag.data", names = cols)
 
-# 1a. Take a look at the dataset
-print(df.head())
+#variable names to use as predictors
+var = [ 'red', 'green', 'blue','gold', 'white', 'black', 'orange', 'mainhue','bars','stripes', 'circles','crosses', 'saltires','quarters','sunstars','triangle','animate']
 
-# 1b. Setting the target and predictor variables
-df['accep'] = ~(df['accep']=='unacc') #1 is acceptable, 0 if not acceptable
-X = pd.get_dummies(df.iloc[:,0:6])
-y = df['accep']
+#Print number of countries by landmass, or continent
+print(df.landmass.value_counts())
 
-# 1c. Examine the new features
-print(X.columns)
-print(len(X.columns))
+#Create a new dataframe with only flags from Europe and Oceania
+df_36 = df[df["landmass"].isin([3,6])]
 
-# 2a. Performing the train-test split
-x_train, x_test, y_train, y_test = train_test_split(X,y, random_state=0, test_size=0.2)
+#Print the average vales of the predictors for Europe and Oceania
+print(df_36.groupby('landmass')[var].mean().T)
 
-# 2b.Fitting the decision tree classifier
-dt = DecisionTreeClassifier(max_depth=3, ccp_alpha=0.01,criterion='gini')
-dt.fit(x_train, y_train)
+#Create labels for only Europe and Oceania
+df_36 = df[df["landmass"].isin([3,6])]
+labels = df_36["landmass"]
 
-# 3.Plotting the Tree
-plt.figure(figsize=(20,12))
-tree.plot_tree(dt, feature_names = x_train.columns, max_depth=5, class_names = ['unacc', 'acc'], label='all', filled=True)
-plt.tight_layout()
+#Print the variable types for the predictors
+print(df[var].dtypes)
+
+#Create dummy variables for categorical predictors
+data = pd.get_dummies(df_36[var])
+
+#Split data into a train and test set
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, random_state=1, test_size=.4)
+
+#Fit a decision tree for max_depth values 1-20; save the accuracy score in acc_depth
+depths = range(1, 21)
+acc_depth = []
+for i in depths:
+    dt = DecisionTreeClassifier(random_state = 10, max_depth = i)
+    dt.fit(train_data, train_labels)
+    acc_depth.append(dt.score(test_data, test_labels))
+
+#Plot the accuracy vs depth
+plt.plot(depths, acc_depth)
+plt.xlabel('max_depth')
+plt.ylabel('accuracy')
 plt.show()
+
+#Find the largest accuracy and the depth this occurs
+
+
+#Refit decision tree model with the highest accuracy and plot the decision tree
+
+
+#Create a new list for the accuracy values of a pruned decision tree.  Loop through
+#the values of ccp and append the scores to the list
+
+
+#Plot the accuracy vs ccp_alpha
+
+
+#Find the largest accuracy and the ccp value this occurs
+
+
+#Fit a decision tree model with the values for max_depth and ccp_alpha found above
+
+
+#Plot the final decision tree
