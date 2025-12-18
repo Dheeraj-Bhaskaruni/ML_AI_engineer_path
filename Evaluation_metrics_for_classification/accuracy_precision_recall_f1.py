d2caa5e445b8f2df75eaf7e786f3f1dba3171c7a
Dheeraj Bhaskaruni <dheeraj58799@gmail.com>
2025-12-18 11:26:38 -0600

    eval metrics

---
+++ Evaluation_metrics_for_classification/accuracy_precision_recall_f1.py
@@ -0,0 +1,26 @@
+actual = [1, 0, 0, 1, 1, 1, 0, 1, 1, 1]
+predicted = [0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+
+for i in range(len(predicted)):
+  if actual[i] == 1 and predicted[i] == 1:
+    true_positives += 1
+  if actual[i] == 0 and predicted[i] == 0:
+    true_negatives += 1
+  if actual[i] == 0 and predicted[i] == 1:
+    false_positives += 1
+  if actual[i] == 1 and predicted[i] == 0:
+    false_negatives += 1
+
+accuracy = (true_positives + true_negatives)/ (true_positives + true_negatives+false_positives + false_negatives)
+print(accuracy)
+recall = true_positives/(true_positives + false_negatives)
+precision = true_positives/(true_positives+false_positives)
+
+f_1 = (2*recall*precision)/(recall+ precision)
+
+print(f_1)
